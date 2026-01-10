f6b9f485998dbb75f0f072612164608b0014a462
ManuelBayonas <119498525+ManuelBayonas@users.noreply.github.com>
2026-01-10 08:47:18 +0100

    Update GravityWell.js

--- entities/GravityWell.js
+++ entities/GravityWell.js
@@ -1,27 +1,27 @@
 export class GravityWell {
-
   constructor(x, y, mass, radius) {
     this.pos = createVector(x, y);
     this.mass = mass;
     this.radius = radius;
-    this.G = 1;
   }
 
   attract(ship) {
-    const force = p5.Vector.sub(this.pos, ship.pos);
+    let force = p5.Vector.sub(this.pos, ship.pos);
     let d = constrain(force.mag(), 25, 300);
     force.normalize();
-    force.mult((this.G * this.mass) / (d * d));
+    let strength = this.mass / (d * d);
+    force.mult(strength);
     return force;
   }
 
-  display(ship) {
-    const d = p5.Vector.dist(this.pos, ship.pos);
-    const alpha = map(d, 300, this.radius, 0, 120, true);
+  collides(ship) {
+    return p5.Vector.dist(this.pos, ship.pos) < this.radius + ship.size * 0.4;
+  }
 
+  display(ship) {
     noFill();
-    stroke(200, alpha);
-    ellipse(this.pos.x, this.pos.y, this.radius * 8);
+    stroke(200, 120);
+    ellipse(this.pos.x, this.pos.y, this.radius * 6);
     fill(180);
     noStroke();
     ellipse(this.pos.x, this.pos.y, this.radius * 2);
