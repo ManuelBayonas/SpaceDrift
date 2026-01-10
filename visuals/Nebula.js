bcf2d1f0e2a3b415e4a96f1db8ddabce09e80694
ManuelBayonas <119498525+ManuelBayonas@users.noreply.github.com>
2026-01-10 08:47:45 +0100

    Update Nebula.js

--- visuals/Nebula.js
+++ visuals/Nebula.js
@@ -1,14 +1,22 @@
 export class Nebula {
 
   render(pg) {
+    pg.beginDraw();
     pg.loadPixels();
+
     for (let y = 0; y < pg.height; y++) {
       for (let x = 0; x < pg.width; x++) {
         const n = noise(x * 0.002, y * 0.002);
-        const c = color(40 + 120 * n, 30 + 60 * n, 90 + 140 * n);
+        const c = color(
+          40 + 120 * n,
+          30 + 60 * n,
+          90 + 140 * n
+        );
         pg.set(x, y, c);
       }
     }
+
     pg.updatePixels();
+    pg.endDraw();
   }
 }
