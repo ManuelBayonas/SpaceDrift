0a0ae4ccf82c9bbbe3c02aea872d5cdfb1cac6b6
ManuelBayonas <119498525+ManuelBayonas@users.noreply.github.com>
2026-01-11 18:54:36 +0100

    Update InputManager.js

--- core/InputManager.js
+++ core/InputManager.js
@@ -1,49 +1,38 @@
+import { LOGICAL_W, LOGICAL_H, gameScale, offsetX, offsetY } from "../sketch.js";
+
 export class InputManager {
 
   constructor() {
-    this.thrusting = false;
     this.dir = createVector(0, 0);
+    this.thrusting = false;
 
-    this.initKeyboard();
-    this.initTouch();
-  }
-
-  /* -------------------------
-     TECLADO
-  ------------------------- */
-  initKeyboard() {
     window.addEventListener("keydown", e => this.onKey(e, true));
     window.addEventListener("keyup",   e => this.onKey(e, false));
-  }
 
-  onKey(e, pressed) {
-    switch (e.code) {
-      case "ArrowUp":    this.dir.y = pressed ? -1 : 0; break;
-      case "ArrowDown":  this.dir.y = pressed ?  1 : 0; break;
-      case "ArrowLeft":  this.dir.x = pressed ? -1 : 0; break;
-      case "ArrowRight": this.dir.x = pressed ?  1 : 0; break;
-    }
-    this.thrusting = this.dir.mag() > 0;
-  }
-
-  /* -------------------------
-     TÁCTIL (móvil)
-  ------------------------- */
-  initTouch() {
     window.addEventListener("touchstart", e => this.onTouch(e));
     window.addEventListener("touchmove",  e => this.onTouch(e));
     window.addEventListener("touchend",   () => this.reset());
+  }
+
+  onKey(e, down) {
+    switch (e.code) {
+      case "ArrowUp":    this.dir.y = down ? -1 : 0; break;
+      case "ArrowDown":  this.dir.y = down ?  1 : 0; break;
+      case "ArrowLeft":  this.dir.x = down ? -1 : 0; break;
+      case "ArrowRight": this.dir.x = down ?  1 : 0; break;
+    }
+    this.thrusting = this.dir.mag() > 0;
   }
 
   onTouch(e) {
     const t = e.touches[0];
     if (!t) return;
 
-    const cx = width / 2;
-    const cy = height / 2;
+    const x = (t.clientX - offsetX) / gameScale;
+    const y = (t.clientY - offsetY) / gameScale;
 
-    this.dir.x = constrain((t.clientX - cx) / cx, -1, 1);
-    this.dir.y = constrain((t.clientY - cy) / cy, -1, 1);
+    this.dir.x = constrain((x - LOGICAL_W / 2) / (LOGICAL_W / 2), -1, 1);
+    this.dir.y = constrain((y - LOGICAL_H / 2) / (LOGICAL_H / 2), -1, 1);
 
     this.thrusting = true;
   }
@@ -52,11 +41,6 @@ export class InputManager {
     this.dir.set(0, 0);
     this.thrusting = false;
   }
-
-  /* -------------------------
-     API pública
-  ------------------------- */
-  update() {}
 
   isThrusting() {
     return this.thrusting;
