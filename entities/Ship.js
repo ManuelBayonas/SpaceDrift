1009d0934379e1cd1c2c055ba9828446a81d2e33
ManuelBayonas <119498525+ManuelBayonas@users.noreply.github.com>
2026-01-11 08:18:03 +0100

    Update Ship.js

--- entities/Ship.js
+++ entities/Ship.js
@@ -37,16 +37,48 @@ thrust(dir) {
 }
   }
 
-  display() {
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.vel.heading());
-    fill(255);
-    noStroke();
+  display(isThrusting = false) {
+  const angle = this.vel.heading();
 
-    triangle(-10, -8, 10, 0, -10, 8);
-    pop();
+  push();
+  pushStyle();
+
+  translate(this.pos.x, this.pos.y);
+  rotate(angle);
+
+  noStroke();
+  fill(255);
+
+  // Cuerpo
+  triangle(
+    -this.size * 0.8, -this.size * 0.8,
+     this.size * 0.8,  0,
+    -this.size * 0.8,  this.size * 0.8
+  );
+
+  // Nariz
+  triangle(
+    -this.size * 0.2, -this.size * 0.45,
+     this.size * 1.1,  0,
+    -this.size * 0.2,  this.size * 0.45
+  );
+
+  // --------------------
+  // ESTELA DE THRUST
+  // --------------------
+  if (isThrusting) {
+    stroke(255, 120);
+    strokeWeight(1);
+    line(
+      -this.size * 0.9, 0,
+      -this.size * (1.4 + random(0.3)), 0
+    );
   }
+
+  popStyle();
+  pop();
+}
+
 
   outOfBounds(w, h) {
     return this.pos.x < 0 || this.pos.x > w ||
